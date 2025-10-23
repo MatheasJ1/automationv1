1116ea132940440c48bc4ce992a6b26c0d06cbf8
Matheas <matheasjarrouj@gmail.com>
2025-10-23 13:09:31 -0400

    33-37 changed the update method added new method calculate nextState, tried different ways of calculating value of next state.

--- Automaton.java
+++ Automaton.java
@@ -13,7 +13,7 @@ public class Automaton
     // The state of the cells.
     private int[] state;
     
-    /**
+/**
      * Create a 1D automaton consisting of the given number of cells.
      * @param numberOfCells The number of cells in the automaton.
      */
@@ -21,22 +21,17 @@ public class Automaton
     {
         this.numberOfCells = numberOfCells;
         state = new int[numberOfCells];
-        // Seed the automaton with a single 'on' cell in the middle.
+        state = new int[numberOfCells + 1];
         state[numberOfCells / 2] = 1;
     }
     
-    /**
+/**
      * Print the current state of the automaton.
      */
     public void print()
     {
-        for(int cellValue : state) {
-            if(cellValue == 1) {
-                System.out.print("*");
-            }
-            else {
-                System.out.print(" ");
-            }
+        for(int i=0; i< numberOfCells; i++){
+            System.out.print(state[i] == 1 ? "*" : " ");
         }
         System.out.println();
     }   
@@ -46,29 +41,32 @@ public class Automaton
      */
     public void update()
     {
-        // Build the new state in a separate array.
         int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left = (i==0) ? 0 : state[i-1];
-            int center = state[i];
-            int right = (i + 1< state.length) ? state[i+1] : 0;
-            state[i] = (left + center + right) % 2;
-        }
+        
+        int left = 0;
+        int center = state[0];
+    for (int i=0; i<numberOfCells; i++){
+        int right = state[i+1];
+        nextState[i] = calculateNextState(left,center,right);
+        left = center;
+        center = right;
         
     }
-    
-    /**
+    state = nextState;
+}
+
+private int calculateNextState(int left, int center, int right){
+        return (left + center + right) % 2;
+    }
+/**
      * Reset the automaton.
      */
     public void reset()
     {
         Arrays.fill(state, 0);
         int middle = numberOfCells / 2;
-        
-        state[middle -1] = 1;
+        if (middle - 1 >=0) state[middle - 1] = 1;
         state[middle] = 1;
-        state[middle + 1] = 1;
+        if (middle + 1 < numberOfCells) state[middle + 1] = 1;
     }
 }
